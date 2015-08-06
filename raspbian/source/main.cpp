fede5790df6e97f643ebfaa9c97230f0529a4592
TEST <test@test.com>
2015-08-06 12:23:31 +0900

    update

--- raspbian/source/main.cpp
+++ raspbian/source/main.cpp
@@ -9,7 +9,7 @@ int main(void){
 
     uint8_t packet[2];
     uint8_t *rxbuf;
-
+    int d = 0;
     
     packet[0] = 0xaa;
     packet[1] = 0x00;
@@ -17,6 +17,9 @@ int main(void){
     while(true){
         for(int i = 0;i <= 1;i++){
             rxbuf = spi->transfer(1,&packet[i]);
+            d = (rxbuf[1] & 0x0f) << 8;
+            d |= rxbuf[2];
+            delete(rxbuf);
             if(i == 0){
                 printf("send 0xaa\n");
             }
